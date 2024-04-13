082c4d49c27bad15e10a9981383769751d3f7107
Billy Charlton <billy@okbecause.com>
2024-04-13 11:22:53 +0200

    Fetch ZSTD-compressed sqlite archive

--- js/tmip-archive-app.js
+++ js/tmip-archive-app.js
@@ -2,10 +2,45 @@
 
 // TMIP Archive Vue Application
 // Questions / PRs, contact Billy Charlton <mail@billyc.cc>
+// ----------------------------------------------------------------
+
+// ZSTD archive decompressor (database is compressed)
+import { ZSTDDecoder } from 'https://unpkg.com/zstddec@0.1.0/dist/zstddec.esm.js';
+
+const DB_PATH = '/tmip-archive/tmip.db.zst';
 
 export default {
   data() {
-    return { count: 0 };
+    return {
+      count: 0,
+      db: {},
+    };
   },
-  template: `<div>count is {{ count }}</div>`,
+
+  mounted() {
+    // this happens async
+    this.db = this.fetchSqliteDatabase();
+  },
+
+  methods: {
+    async fetchSqliteDatabase() {
+      // initialize ZSTD decompressor
+      const decoder = new ZSTDDecoder();
+      await decoder.init();
+
+      // fetch & uncompress archive
+      const archive = await (await fetch(DB_PATH)).arrayBuffer();
+      const buffer = decoder.decode(new Uint8Array(archive));
+      console.log('BUFFER bytes', buffer.byteLength);
+
+      this.count++;
+      console.log({ archive, buffer });
+
+      return buffer;
+    },
+  },
+
+  template: `
+    <div>count is {{ count }}</div>
+  `,
 };
